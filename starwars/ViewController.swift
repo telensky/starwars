f35b4fcf6cecc2dbf1d31c62fdb74644c3f34a3c
telensky <telensky01@southhills.edu>
2017-12-13 08:32:04 -0800

    Initial Commit

---
+++ starwars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  starwars
+//
+//  Created by Tanner Elensky on 12/13/17.
+//  Copyright © 2017 Tanner Elensky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
