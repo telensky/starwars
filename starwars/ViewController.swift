366bdb00b19e77e229770cc914078a4a51b8ecb3
telensky <telensky01@southhills.edu>
2018-01-03 08:20:39 -0800

    .

--- starwars/ViewController.swift
+++ starwars/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // so I can make images appear when a button is clicked, made it private.
+    @IBOutlet private var imageview: UIImageView!
+    
+    // so I can add a custom message in my text field on top of the picture it is displaying.
+    @IBOutlet private var textview: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // if button clicked should display blue with message ray choose the light side
+    @IBAction func rayjedibutton(_ sender: Any) {
+        imageview.image = #imageLiteral(resourceName: "BlueLightSaber.png")
+        textview.text = "Ray choose the light side"
     }
-
-
+    // if button clicked should dispaly red with message ray choose the dark side
+    @IBAction func raysithbutton(_ sender: Any) {
+        imageview.image = #imageLiteral(resourceName: "RedLightSaber.png")
+        textview.text = "Ray choose the dark side"    }
+    // if button clicked should display blue kylo choose the light side
+    @IBAction func kylojedibutton(_ sender: Any) {
+        imageview.image = #imageLiteral(resourceName: "BlueLightSaber.png")
+        textview.text = "Kylo choose the light side"    }
+    // if button clicked should display red kylo choose the dark side
+    @IBAction func kylosithbutton(_ sender: Any) {
+        imageview.image = #imageLiteral(resourceName: "RedLightSaber.png")
+        textview.text = "Kylo choose the dark side"    }
+    
 }
 
